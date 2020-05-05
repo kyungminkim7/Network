874c267eadaa14725fd18c5cdeefbf88d8b1e7c0
k <kyungminkim7@gmail.com>
2020-05-05 05:34:28 -0500

    Added geometry_msgs

--- tests/Client.cpp
+++ tests/Client.cpp
@@ -1,3 +1,27 @@
+#include <iostream>
+
+#include <asio.hpp>
+
+#include <geometry_msgs/Vector3_generated.h>
+#include <std_msgs/Header_generated.h>
+#include <network/TcpSubscriber.h>
+
+void onVector3Received(std::unique_ptr<uint8_t[]> vec3Buffer) {
+    auto vec3 = geometry_msgs::GetVector3(vec3Buffer.get());
+    std::cout << "(" << vec3->x() << ", " << vec3->y() << ", " << vec3->z() << ")\n";
+}
+
 int main(int argc, char *argv[]) {
+    asio::io_context ioContext;
+
+    auto vec3Subscriber = ntwk::TcpSubscriber::create(ioContext, "127.0.0.1", 4444, &onVector3Received);
+    auto headerSubscriber = ntwk::TcpSubscriber::create(ioContext, "127.0.0.1", 5555, [](auto headerBuffer){
+        auto header = reinterpret_cast<std_msgs::Header*>(headerBuffer.get());
+        std::cout << "Header: " << header->msgSize() << "\n";
+    });
+
+
+    ioContext.run();
+
     return 0;
 }
--- tests/Server.cpp
+++ tests/Server.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
+#include <memory>
 
 #include <asio.hpp>
+
+#include <geometry_msgs/Vector3_generated.h>
+#include <std_msgs/Header_generated.h>
 #include <network/TcpPublisher.h>
 
 int main(int argc, char *argv[]) {
     asio::io_context ioContext;
 
-    auto publisher = ntwk::TcpPublisher::create(ioContext, 4444);
+    auto vec3Publisher = ntwk::TcpPublisher::create(ioContext, 4444);
+    auto headerPublisher = ntwk::TcpPublisher::create(ioContext, 5555);
 
     int x;
 
-    while (true) {
+    while (x != 0) {
         std::cout << "enter a number: ";
         std::cin >> x;
 
-        publisher->publish();
+        flatbuffers::FlatBufferBuilder msgBuilder(64);
+        auto vec = geometry_msgs::CreateVector3(msgBuilder, 1.1f, 2.2f, 3.3f);
+        msgBuilder.Finish(vec);
+
+        vec3Publisher->publish(std::make_shared<flatbuffers::DetachedBuffer>(msgBuilder.Release()));
+        headerPublisher->publish(std::make_shared<std_msgs::Header>(5849));
 
         ioContext.poll();
         ioContext.restart();
