b6ed37e958961aa74430000003a56f3070d65505
Your Name <you@example.com>
2021-10-27 11:26:47 -0500

    Changed Node to be the ultimate Facade in keeping track of multiple subscribers (to potentially different machines) as well as it's own publisher

--- include/network/Node.h
+++ include/network/Node.h
@@ -2,10 +2,13 @@
 
 #include <cstdint>
 #include <functional>
+#include <map>
 #include <memory>
 #include <thread>
+#include <utility>
 
 #include <asio/io_context.hpp>
+#include <flatbuffers/flatbuffers.h>
 
 namespace ntwk {
 
@@ -14,12 +17,18 @@ class TcpSubscriber;
 
 class Node {
 public:
+    using Endpoint = std::pair<std::string, unsigned short>;
+    using PublisherPtr = std::shared_ptr<TcpPublisher>;
+    using SubscriberPtr = std::shared_ptr<TcpSubscriber>;
+
     Node();
     ~Node();
 
-    std::shared_ptr<TcpPublisher> advertise(unsigned short port);
-    std::shared_ptr<TcpSubscriber> subscribe(const std::string &host, unsigned short port,
-                                             std::function<void(std::unique_ptr<uint8_t[]>)> msgReceivedHandler);
+    void advertise(unsigned short port);
+    void subscribe(const std::string &host, unsigned short port,
+                   std::function<void(std::unique_ptr<uint8_t[]>)> msgReceivedHandler);
+
+    void publish(std::shared_ptr<flatbuffers::DetachedBuffer> msg);
 
     void run();
     void runOnce();
@@ -29,6 +38,10 @@ private:
     asio::io_context tasksContext;
 
     std::thread tasksThread;
+
+    std::map<Endpoint, SubscriberPtr> subscribers;
+    PublisherPtr publisher;
+
 };
 
 } // namespace ntwk
--- src/Node.cpp
+++ src/Node.cpp
@@ -18,14 +18,24 @@ Node::~Node() {
     this->tasksThread.join();
 }
 
-std::shared_ptr<TcpPublisher> Node::advertise(unsigned short port) {
-    return TcpPublisher::create(this->tasksContext, port);
+void Node::advertise(unsigned short port) {
+    this->publisher = TcpPublisher::create(this->tasksContext, port);
 }
 
-std::shared_ptr<TcpSubscriber> Node::subscribe(const std::string &host, unsigned short port,
-                                               std::function<void(std::unique_ptr<uint8_t[]>)> msgReceivedHandler) {
-    return TcpSubscriber::create(this->mainContext, this->tasksContext,
-                                 host, port, std::move(msgReceivedHandler));
+void Node::subscribe(const std::string &host, unsigned short port,
+                     std::function<void(std::unique_ptr<uint8_t[]>)> msgReceivedHandler) {
+    auto endpoint = std::make_pair(host, port);
+    auto &s = this->subscribers[endpoint];
+    if (!s) {
+        s = TcpSubscriber::create(this->mainContext, this->tasksContext,
+                                  host, port, std::move(msgReceivedHandler));
+    } else {
+        // TODO subscribe
+    }
+}
+
+void Node::publish(std::shared_ptr<flatbuffers::DetachedBuffer> msg) {
+    this->publisher->publish(std::move(msg));
 }
 
 void Node::run() {
